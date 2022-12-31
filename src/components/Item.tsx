3c5067e519fa7feace6aadc9e07e1adc6244e886
Chad <chadjcampbell@gmail.com>
2022-12-31 14:14:49 -0500

    bootstrap image cards

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,3 +1,13 @@
+import { Card } from "react-bootstrap";
+
 export default function Item({ ...item }) {
-  return <h6>{item.title}</h6>;
+  return (
+    <Card>
+      <Card.Img
+        variant="top"
+        src={item.image}
+        style={{ objectFit: "contain", height: "200px", width: "200px" }}
+      />
+    </Card>
+  );
 }
