cdc4d3bb2859fe0eff841ab7bbb2e7e340b0b129
Chad <chadjcampbell@gmail.com>
2022-12-31 13:35:06 -0500

    fetch items from API and map to components

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,3 +1,30 @@
+import { useState, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import Item from "../components/Item";
+
 export default function Shop() {
-  return <h2>Shop</h2>;
+  const [items, setItems] = useState([]);
+  const fetchItems = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setItems(data);
+  };
+
+  useEffect(() => {
+    fetchItems();
+    console.log(items);
+  }, []);
+
+  return (
+    <>
+      <h2>Shop</h2>
+      <Row className="g-3">
+        {items.map((item) => (
+          <Col key={item.id}>
+            <Item {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
