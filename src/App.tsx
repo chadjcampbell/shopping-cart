325ceb0ab4fd6f5ecac92da93000314dcb315dff
Chad <chadjcampbell@gmail.com>
2023-01-04 09:56:36 -0500

    create ShoopingCart context and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Container from "react-bootstrap/Container";
+import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Container className="mb-4">
         <Outlet />
       </Container>
-    </>
+    </CartProvider>
   );
 }
