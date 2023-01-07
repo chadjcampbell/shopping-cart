2cc6341de2b67bbb2ba1f77c786232baf750d219
Chad <chadjcampbell@gmail.com>
2023-01-07 14:38:51 -0500

    animate contacts page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import { ContactCard } from "../components/ContactCard";
 
@@ -7,16 +8,28 @@ export default function Contact() {
       <h2 className="text-center mt-3">Contact Us</h2>
       <Container>
         <Row xs={1} md={2}>
-          <Col className="mt-3">
-            <Card className="mx-auto">
-              <ContactCard />
-            </Card>
-          </Col>
-          <Col className="mt-3">
-            <Card className="mx-auto">
-              <img src="../public/fakemap.jpg" />
-            </Card>
-          </Col>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5, x: -200 }}
+            animate={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Col className="mt-3 shadow">
+              <Card className="mx-auto">
+                <ContactCard />
+              </Card>
+            </Col>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5, x: 200 }}
+            animate={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Col className="mt-3 shadow">
+              <Card className="mx-auto">
+                <img src="../public/fakemap.jpg" />
+              </Card>
+            </Col>
+          </motion.div>
         </Row>
       </Container>
     </Container>
