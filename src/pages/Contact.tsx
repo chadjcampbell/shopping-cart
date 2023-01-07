d41915f0feed433759ddcaf3a5917b2c06df34c6
Chad <chadjcampbell@gmail.com>
2023-01-07 14:22:19 -0500

    make basic contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,24 @@
+import { Card, Container, Row, Col } from "react-bootstrap";
+import { ContactCard } from "../components/ContactCard";
+
 export default function Contact() {
-  return <h2>Contact</h2>;
+  return (
+    <Container>
+      <h2 className="text-center mt-3">Contact Us</h2>
+      <Container>
+        <Row xs={1} md={2}>
+          <Col className="mt-3">
+            <Card className="mx-auto">
+              <ContactCard />
+            </Card>
+          </Col>
+          <Col className="mt-3">
+            <Card className="mx-auto">
+              <img src="../public/fakemap.jpg" />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
